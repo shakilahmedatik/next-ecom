9b0139dc259ff4fc05c2336163a68f4ba8313282
Shakil Ahmed Atik <shakilatik@gmail.com>
2023-01-19 16:10:38 +0600

    initial setup and db configuration done

---
+++ utils/db.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+mongoose.set('strictQuery', false)
+const connection = {}
+
+export async function connectDB() {
+  if (connection.isConnected) {
+    console.log('Already connected to the database.')
+    return
+  }
+  if (mongoose.connections.length > 0) {
+    connection.isConnected = mongoose.connections[0].readyState
+    if (connection.isConnected === 1) {
+      console.log('Use previous connection of the database.')
+      return
+    }
+    const db = await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    console.log('New connection to the database')
+    connection.isConnected = db.connections[0].readyState
+  }
+}
